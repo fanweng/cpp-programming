4a6fe6d38f6938cb9991ab724a9d91d8806d57bd
Fan Weng <fanweng02@gmail.com>
2020-09-18 23:10:16 -0400

    Add ON_CALL, Invoke, DoAll code into the example.

--- google-test/mock/mock01.cpp
+++ google-test/mock/mock01.cpp
@@ -4,6 +4,10 @@
 
 using namespace std;
 using ::testing::Return;
+using ::testing::_;
+using ::testing::AtLeast;
+using ::testing::Invoke;
+using ::testing::DoDefault;
 
 class DataBaseConnect {
 
@@ -62,18 +66,62 @@ private:
     DataBaseConnect &dbC;
 };
 
-TEST(MyDBTest, loginTest) {
+struct StructA {
+    bool dummyLogin1(string a, string b) {
+        cout << "dummyLogin1 gets called!" << endl;
+        return true;
+    }
+}
+
+struct StructB {
+    void dummyLogin2(string a, string b) {
+        cout << "dummyLogin2 gets called!" << endl;
+        return;
+    }
+}
+
+TEST(MyDBTest, loginTest1) {
 
     MockDB mdb;
     MyDataBase db(mdb);
 
-    // EXPECT_CALL(mdb, login(_, _))   // "_" means anything input is OK
     EXPECT_CALL(mdb, login("mike", "12345"))
     .Times(1)
     .WillOnce(Return(true));
 
     /* ON_CALL allows the login2() may not be called */
     ON_CALL(mdb, login2(_,_)).WillByDefault(Return(true));
+
+    int ret = db.Init("mike", "12345");
+    EXPECT_EQ(ret, 0);
+}
+
+TEST(MyDBTest, loginTest2) {
+
+    MockDB mdb;
+    MyDataBase db(mdb);
+    StructA dbTest;
+
+    /* "_" means anything input is OK */
+    /* Invoke() actually can call anything methods from anywhere */
+    ON_CALL(mdb, login(_,_)).WillByDefault(Invoke(&dbTest, &StructA::dummyLogin1));
+    EXPECT_CALL(mdb, login(_, _))
+    .Times(AtLeast(1))
+    .WillOnce(DoDefault());
+
+    int ret = db.Init("mike", "12345");
+    EXPECT_EQ(ret, 0);
+}
+
+TEST(MyDBTest, loginTest3) {
+
+    MockDB mdb;
+    MyDataBase db(mdb);
+    StructB dbTest;
+
+    EXPECT_CALL(mdb, login(_, _))
+    .Times(AtLeast(1))
+    .WillOnce(DoAll(Invoke(&dbTest, &StructB::dummyLogin2), Invoke(&dbTest, &StructB::dummyLogin2), Return(true))); // only the last action of DoAll() can return a value
 
     int ret = db.Init("mike", "12345");
     EXPECT_EQ(ret, 0);
